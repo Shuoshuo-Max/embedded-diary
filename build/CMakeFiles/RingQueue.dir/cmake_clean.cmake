1e7f38ed9924731fcecaef8810f6916618058dbf
ShuoshuoMax <2119069237@qq.com>
2025-10-02 16:43:15 +0800

    Oct-2: coverage ≥90% - uploaded coverage data to codecov

---
+++ build/CMakeFiles/RingQueue.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RingQueue.dir/src/RingQueue.cpp.obj"
+  "CMakeFiles/RingQueue.dir/src/RingQueue.cpp.obj.d"
+  "RingQueue.exe"
+  "RingQueue.exe.manifest"
+  "RingQueue.pdb"
+  "libRingQueue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RingQueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/RingQueue.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RingQueue.
+# This may be replaced when dependencies are built.
